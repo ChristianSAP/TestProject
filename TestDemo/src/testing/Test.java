1a81b51a608acebdaff4cc4cbcea069ecb4aba4f
Jana Lotta Pfeffer <jana.lotta.pfeffer01@sap.com>
2018-02-01 11:27:57 +0100

    Inner Join results are rpinted out

--- TestDemo/src/testing/Test.java
+++ TestDemo/src/testing/Test.java
@@ -4,16 +4,14 @@ import java.sql.*;
 
 public class Test {
 	public static void main(String[] args) {
-		Date temp = new Date(0);
-		System.out.println(temp);
 		// variables for HANA db connection
 		Connection connection = null;
 		String myname = "ETD_TEST_CLIENT";
 		String mysecret = "Initial04";
 		// variables to store information about current log
 		String timeStamp = "20.01.2018 03:00:00.0";
-		String systemIdActor = "YI3/000";
-		String userIdActor = "00505695007C1ED786A8AB36D6125FF6";
+		String systemIdActor = "$T3/000";
+		String userIdActor = "552F9FEC6D382BA3E10000000A4CF109";
 		try { // opening connection to HANA db
 			connection = DriverManager.getConnection("jdbc:sap://ld3796.wdf.sap.corp:30015/?autocommit=false", myname,
 					mysecret);
@@ -22,13 +20,15 @@ public class Test {
 			return;
 		}
 		// todo get log to analysis, do something with the result! -> score
-		//analysisUnusualTime(connection, systemIdActor, timeStamp, userIdActor);
+		// analysisUnusualTime(connection, systemIdActor, timeStamp,
+		// userIdActor);
 		analysisUnusualSystem(connection, systemIdActor, timeStamp, userIdActor);
 
 	}
 
 	// analysis: log at unusual time? (based on System ID, not User ID!)
-	public static boolean analysisUnusualTime(Connection connection, String systemIdActor, String timeStamp, String userIdActor) {
+	public static boolean analysisUnusualTime(Connection connection, String systemIdActor, String timeStamp,
+			String userIdActor) {
 		boolean restPeriod = false;
 		long numberOfLogsTwoHours;
 		long numberOfLogsTenMinutes;
@@ -41,7 +41,7 @@ public class Test {
 
 				ResultSet resultSet = stmt.executeQuery(
 						"SELECT COUNT (\"UserIdActing\") FROM \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\" "
-								+ "WHERE \"UserIdActing\" = '" + userIdActor 
+								+ "WHERE \"UserIdActing\" = '" + userIdActor
 								+ "' AND \"Timestamp\" BETWEEN '31.01.2018 12:00:00.0' AND '31.01.2018 14:00:00.0'");
 				resultSet.next();
 				numberOfLogsTwoHours = Integer.parseInt(resultSet.getString(1));
@@ -54,7 +54,7 @@ public class Test {
 				resultSetLast10Minutes.next();
 				numberOfLogsTenMinutes = Integer.parseInt(resultSetLast10Minutes.getString(1));
 				medianTenMinutes = numberOfLogsTenMinutes / 10;
-				
+
 				if (medianTenMinutes < medianTwoHours) {
 					restPeriod = true;
 				} else {
@@ -76,28 +76,38 @@ public class Test {
 		if (connection != null) {
 			try {
 				Statement stmt = connection.createStatement();
-				ResultSet resultSet = stmt.executeQuery("SELECT \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"TechnicalLogEntryType\", \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"SystemIdActor\","
-						+ "\"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"Timestamp\", \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"UserIdActing\","
-						+ "\"SAP_SEC_MON\".\"sap.secmon.db::KnowledgeBase.LogEntryType\".\"eventName.name\""
-						+ "FROM \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\""
-						+ "INNER JOIN \"SAP_SEC_MON\".\"sap.secmon.db::KnowledgeBase.LogEntryType\" "
-						+ "ON \"SAP_SEC_MON\".\"sap.secmon.db::KnowledgeBase.LogEntryType\".\"id\" = \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"TechnicalLogEntryType\""
-						+ "WHERE \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"SystemIdActor\" = '" + systemIdActor + "' AND "
-						+ "\"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"UserIdActing\" = '" + userIdActor + "' AND"
-						+ "\"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"Timestamp\" BETWEEN '01.11.2017 00:00:00.0' AND '31.01.2018 00:00:00.0'");
-				numberOfLogins = Integer.parseInt(resultSet.getString(1));
-				System.out.println(numberOfLogins);
+				ResultSet resultSet = stmt.executeQuery(
+						"SELECT \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"TechnicalLogEntryType\", \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"SystemIdActor\","
+								+ "\"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"Timestamp\", \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"UserIdActing\","
+								+ "\"SAP_SEC_MON\".\"sap.secmon.db::KnowledgeBase.LogEntryType\".\"eventName.name\""
+								+ "FROM \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\""
+								+ "INNER JOIN \"SAP_SEC_MON\".\"sap.secmon.db::KnowledgeBase.LogEntryType\" "
+								+ "ON \"SAP_SEC_MON\".\"sap.secmon.db::KnowledgeBase.LogEntryType\".\"id\" = \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"TechnicalLogEntryType\""
+								+ "WHERE \"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"SystemIdActor\" = '"
+								+ systemIdActor + "' AND "
+								+ "\"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"UserIdActing\" = '" + userIdActor + "' AND"
+								+ "\"SAP_SEC_MON\".\"sap.secmon.db::KnowledgeBase.LogEntryType\".\"eventName.name\" = 'UserLogon' AND "
+								+ "\"SAP_SEC_MON\".\"sap.secmon.db::Log.Events\".\"Timestamp\" BETWEEN '01.11.2017 00:00:00.0' AND '31.01.2018 00:00:00.0'");
+				// numberOfLogins = Integer.parseInt(resultSet.getString(1));
+				while (resultSet.next()) {
+					for (int i = 0; i <= resultSet.getMetaData().getColumnCount(); i++) {
+						if (i > 1) {
+							System.out.print(",  ");
+							String columnValue = resultSet.getString(i);
+							System.out.print(columnValue + " " + resultSet.getMetaData().getColumnName(i));
+
+						}
+						System.out.println("");
+					}
+				}
 				
-				
-				
-				
+
 			} catch (SQLException e) {
 				System.err.println("Query failed!");
 			}
 		}
 		return unusualSystem;
 	}
-	
 
 }
 
